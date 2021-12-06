d4a325a77c198b7c4b5a8be1651fba95bc183fe2
Scott Esbrandt <scott.esbrandt@snyk.io>
2021-12-06 00:22:32 -0500

    remove mm3

--- gradle_kotlin_mm_3/buildSrc/src/main/java/ConfigData.kt
+++
@@ -1,8 +0,0 @@
-object ConfigData {
-    const val COMPILE_SDK_VERSION = 31
-    const val BUILD_TOOLS_VERSION = "30.0.3"
-    const val MIN_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 31
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0"
-}
