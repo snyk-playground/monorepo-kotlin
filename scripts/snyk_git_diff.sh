0b5d1ef5e8d8ddca9e4d956ab5184c7355e5a5ba
scotte-snyk <59706011+scotte-snyk@users.noreply.github.com>
2022-01-25 23:52:56 -0500

    Update snyk_git_diff.sh

--- scripts/snyk_git_diff.sh
+++ scripts/snyk_git_diff.sh
@@ -8,13 +8,12 @@ read -ra snyk_args <<< "$*"
 
 git fetch --no-tags --prune --depth=1 origin +refs/heads/*:refs/remotes/origin/*
 
-#read -ra git_diff <<< $(git --no-pager diff ${SNYK_DIFF_UPSTREAM_REF} HEAD --name-only --diff-filter=d)
-
-#echo "${git_diff[@]}"
+# read -ra git_diff <<< $(git --no-pager diff ${SNYK_DIFF_UPSTREAM_REF} HEAD --name-only --diff-filter=d)
+# echo "${git_diff[@]}"
 
 for diff in $(git --no-pager diff ${SNYK_DIFF_UPSTREAM_REF} HEAD --name-only --diff-filter=d); do
     echo "checking ${diff}"
-    if [ "${SNYK_GIT_DIFF_FILENAME}" == "$(basename ${diffs})" ]; then
-        echo "snyk ${snyk_args[*]} --file=${diffs}"
+    if [ "${SNYK_GIT_DIFF_FILENAME}" == "$(basename ${diff})" ]; then
+        echo "snyk ${snyk_args[*]} --file=${diff}"
     fi
 done
