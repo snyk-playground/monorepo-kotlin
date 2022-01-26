45f3fb0ba2887afc3c63088b15bf7f129f5dede0
scotte-snyk <59706011+scotte-snyk@users.noreply.github.com>
2022-01-25 23:32:32 -0500

    Update snyk_git_diff.sh

--- scripts/snyk_git_diff.sh
+++ scripts/snyk_git_diff.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-declare -x SNYK_GIT_DIFF_UPSTREAM_REF=${SNYK_DIFF_UPSTREAM_REF:=main}
+declare -x SNYK_GIT_DIFF_UPSTREAM_REF=${SNYK_DIFF_UPSTREAM_REF:=origin\/main}
 
 declare -x SNYK_GIT_DIFF_FILENAME="${SNYK_GIT_DIFF_FILENAME:=build.gradle}"
 
 read -ra snyk_args <<< "$*"
 
-read -ra git_diff <<< $(git --no-pager diff ${SNYK_DIFF_UPSTREAM_REF} --name-only --diff-filter=d )
+read -ra git_diff <<< $(git --no-pager diff ${SNYK_DIFF_UPSTREAM_REF} HEAD --name-only --diff-filter=d )
 
 for diffs in "${git_diff[@]}"; do
     if [ "${SNYK_GIT_DIFF_FILENAME}" == "$(basename ${diffs})" ]; then
